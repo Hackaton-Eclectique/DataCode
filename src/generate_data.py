98576b898feee721d55ef58804d5c37382bc0214
AnaAGG <a.garciagar5@gmail.com>
2021-10-20 22:11:26 +0200

    doneeeee🔥

--- src/generate_data.py
+++ src/generate_data.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import datetime
 
-def get_data(date1, date2):
+def get_data_glucose(date1, date2):
     
     #creamos los datos de glucosa para un dia y los hacemos dataframe
     data = []
@@ -24,3 +24,31 @@ def get_data(date1, date2):
     final = final.dropna()
     final.set_index("Date")
     return final
+
+def get_data_hemoglobine(date1, date2):
+    
+    #creamos los datos de glucosa para un dia y los hacemos dataframe
+    data = []
+    for _ in range(1,367):
+        s = pd.DataFrame(np.random.poisson(3, 24))
+        data.append(s)
+    hb = pd.concat(data).reset_index()
+    hb.drop(["index"], inplace = True, axis = 1)
+        
+    # creamos las fechas y las horas (frecuencia de una hora) y convertimos a dataframe
+    mydates = pd.date_range(date1, date2,  freq='H').tolist()
+    df2 = pd.DataFrame(mydates)
+    # concatenamos los dataframes
+    final = pd.concat([hb, df2], axis = 1)
+    
+    # limpieza
+    final.columns = ["Hb", "Date"]
+    
+    final = final.dropna()
+    final.set_index("Date")
+    return final
+
+def clean_date(df, col):
+    df['Dates'] = pd.to_datetime(df[col]).dt.date
+    df['Time'] = pd.to_datetime(df[col]).dt.time
+    return df
